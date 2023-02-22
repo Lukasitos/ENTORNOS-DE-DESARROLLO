8262b85bd173f9810135b184ef85992b0efeee3d
Lukasitos <bynever7@gmail.com>
2023-02-22 15:23:46 +0100

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,34 +1,11 @@
-lista = list(range(6))
+import requests
 
-cadena = "esto es una cadena"
+r = requests.get('http://httpbin.org/status/404')
 
-print(lista[1:-1])
-print(lista[0:-2])
+if r.status_code==404:
+    print("ok")
+    print("tengo un pedazo pollon")
+    print(r.status_code)
 
-print(cadena[4:-5])
-print(cadena[5:len(cadena)])
-
-
-def conjuga(verbo):
-    sol = []
-    raiz = verbo[0:4]
-    termi= verbo[4:len(verbo)]
-    listar = ['o', 'as', 'a', 'amos', 'áis', 'an']
-    if termi == "ar":
-        for i in listar:
-            sol.append(raiz+i)
-    return sol
-print(conjuga("cantar"))
-
-
-import pytest
-
-@pytest.mark.basic
-def test_notas():
-    assert "notable" == test_notas(8)
-    assert "notable" == test_notas(3)
-    assert "sobresaliente" == test_notas(10)
-    assert "suspenso" == test_notas(3)
-    assert "suspenso" == test_notas(-3)
-    assert "bien" == test_notas(6)
-    assert "la nota introducida es erronea" == test_notas(24)
+else:
+    print("joder que asco das puto negrata")
